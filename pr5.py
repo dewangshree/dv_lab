61587460df0fe193c8593bbc059d7f735ec97ff3
Shreyas Dewangswami <138751486+dewangshree@users.noreply.github.com>
2025-02-01 19:03:28 +0530

    pr5.py

--- pr5.py
+++ pr5.py
@@ -1,7 +1,8 @@
 import pandas as pd  
 import matplotlib.pyplot as plt  
 from sklearn.model_selection import train_test_split  
-from sklearn.neighbors import KNeighborsClassifier  
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
 
 # Load the Iris dataset from CSV  
 df = pd.read_csv("iris_dataset.csv")  
